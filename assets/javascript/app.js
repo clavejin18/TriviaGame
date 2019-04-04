23382178ba26033b82be157c172eabebe3540acb
Jaime Perez <clavejin18@gmail.com>
2019-04-03 23:03:56 -0700

    final touches

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,10 +9,9 @@ var counter = 45;
 var openScreen;
 var gameHTML;
 //arrays for game
-var questionArray = [];
-var answerArray = [];
-var imageArray = [];
-var correctAnswers =[];
+var questionArray = ["What is the only U.S. state that only borders one Other?","Name the number that is three more than one-fifth of one-tenth of one-half of 5,000?","What is the missing number of: 1,1,2,3,5,8,13,_,34", "What's the oldest continuously inhabited city in the world?", "What is the only U.S. state with a Spanish motto?"];
+var answerArray = [["Rhode Island", "Maine", "Washington", "Florida"],["503","103","53","108"], ["20", "21", "25", "17"], ["Instabul, Turkey", "Athens, Greece", "Jerusalem", "Damascus, Syria"], ["Idaho", "California", "Montana", "Arizona"]];
+var correctAnswers =["B. Maine","C. 53", "B. 21", "D. Damascus, Syria", "C. Montana"];
 //begin game code
 $(document).ready(function() {
 //opening page
@@ -33,12 +32,10 @@ $("#gameArea").on("click", ".start-button", function(event){
 $("body").on("click", ".answer", function(event){
     answerSelected = $(this).text();
     if (answerSelected === correctAnswers[questionCounter]) {
-        alert("correct");
         clearInterval(clockCountdown);
         generateWin();
         }
         else {
-            alert("wrong answer!");
             clearInterval(clockCountdown);
             generateLoss();
         }
@@ -51,21 +48,21 @@ $("body").on("click", ".answer", function(event){
 //time loss function
 function timeoutLoss() {
     numberNotAnswered++;
-    gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>You ran out of time!  The correct answer was: " + correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='#'>";
+    gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>You ran out of time!  The correct answer was: " + correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='https://media.giphy.com/media/3ornjXizVZDbngmjRK/giphy.gif'>";
     $("#gameArea").html(gameHTML);
     setTimeout(wait, 4500);
 }
 // win function
 function generateWin() {
     numberCorrect++;
-    gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Correct! The answer is: " + correctAnswers[questionCounter] + "</p>" + imageArray[questionCounter];
+    gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Correct! The answer is: " + correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-win' src='https://media.giphy.com/media/26tPcgtbhhbU88U2A/giphy.gif'>";
     $("#gameArea").html(gameHTML);
     setTimeout(wait, 3000);
 }
 // loss function
 function generateLoss() {
     numberIncorrect++;
-    gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Wrong! The correct answer is: "+ correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='#'>";
+    gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Wrong! The correct answer is: "+ correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='https://media.giphy.com/media/GW10shdM3oXok/giphy.gif'>";
     $("#gameArea").html(gameHTML);
     setTimeout(wait, 3000); 
 }
@@ -76,7 +73,7 @@ function generateQuestions() {
 };
 // wait function
 function wait() {
-    if (questionCounter < 5) {
+    if (questionCounter < 4) {
     questionCounter++,
     generateQuestions();
     counter = 45;
